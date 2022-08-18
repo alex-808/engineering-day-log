c9ef49236b70f37310a61adf5fbdfb782d0a3546
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-08-17 22:21:57 -0700

    Add Tag model, seeder etc

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('notes', NoteController::class);
+Route::apiResource('tags', TagController::class);
