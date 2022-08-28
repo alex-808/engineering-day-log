48da4219deaf2fc3944a5036b1a93acb5154514b
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-08-28 11:10:10 -0700

    Add resetting passwords

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
-Route::middleware(
-	'auth'
-)->prefix('v1')->group(function () {
-	Route::apiResource('notes', NoteController::class);
-	Route::apiResource('tags', TagController::class);
-	Route::apiResource('users', UserController::class);
-});
+Route::apiResource('notes', NoteController::class);
+Route::apiResource('tags', TagController::class);
+Route::apiResource('users', UserController::class);
