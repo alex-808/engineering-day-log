a923508de9135fe7e7a7cc98922c914efd6cf9e3
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-08-27 23:16:57 -0700

    Add registration

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
+
 Route::apiResource('notes', NoteController::class);
 Route::apiResource('tags', TagController::class);
 Route::apiResource('users', UserController::class);
